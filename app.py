33ace4fc324e5194ae789a0a37d62a5b400ffdba
Travis Tuttle <tatuttle2015@gmail.com>
2020-11-01 16:42:24 -0500

    introducing html css, and js.

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template,session, redirect, url_for, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route("/index")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
