89b34a2f28fabf96301289d9c9a22257f3d677b4
Travis Tuttle <tatuttle2015@gmail.com>
2020-11-02 21:03:01 -0500

    Aesthetic updates

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/graphs")
+def graphs():
+    return render_template("graphs.html")
+
+@app.route("/maps")
+def maps():
+    return render_template("maps.html")
+
 @app.route("/about")
 def about():
     return render_template("about.html")
